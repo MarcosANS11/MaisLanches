9433d2dd3eb72a731b62d27ee7d0a4e3ccb7410b
MarcosANS11 <86157931+MarcosANS11@users.noreply.github.com>
2021-08-31 00:44:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,27 @@ for (const element of list) {
     nav.classList.remove('show')
   })
 }
+
+//===============PEDIDO===================//
+function order() {
+  var nome = document.getElementById('nome').value
+  var pedido = document.getElementById('pedido').value
+  var numero = document.getElementById('numero').value
+  var address = document.getElementById('address').value
+  if ((nome == '') | (pedido == '') | (numero == '') | (address == '')) {
+    alert('Preencha todos os espaços do pedido')
+    document.getElementById('botao-pedido').target = ''
+    document.getElementById('botao-pedido').href = '#cardapio-order'
+  } else {
+    document.getElementById('botao-pedido').target = '_blank'
+    document.getElementById('botao-pedido').href =
+      'https://api.whatsapp.com/send?phone=5582981571008&text=Nome: ' +
+      nome +
+      ' Contato: ' +
+      numero +
+      ' Endereço: ' +
+      address +
+      ' Pedido: ' +
+      pedido
+  }
+}
